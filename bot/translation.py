217cf07ee51e8fafd52183ace31f8ca2102227fa
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-06-10 20:28:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -23,5 +23,5 @@ MY OWNER IS NEGAN.ONLY HE CAN USE ME 😪
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/DheerajReji/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/DheerajReji/Advanced-auto-filter-bot">Click Me</a></i>
 """
