486228b9645ad622507d99f3c4deb43995299e96
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-06-10 20:07:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developer', url="https://t.me/the_johnniewalker"
                                 )
                         ]
                     ]
