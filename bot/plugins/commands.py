ab97329059395c84a2b78a15dedee856c8431358
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-06-11 12:35:57 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -92,8 +92,6 @@ async def start(bot, update):
         InlineKeyboardButton('🤍 MY DEVELOPER 🤍', url='https://t.me/the_johnniewalker')
     ],[
         InlineKeyboardButton('🔆JOIN OUR GROUP🔆', url='https://t.me/joinchat/NfFdC-5j5YxhMTc1')
-    ],[
-        InlineKeyboardButton('⚙️ HELP ⚙️', callback_data="help")
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -102,25 +100,6 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(
                 update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-
-
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
-async def help(bot, update):
-    buttons = [[
-        InlineKeyboardButton('Home ⚡', callback_data='start') 
-    ],[
-        InlineKeyboardButton('Close 🔐', callback_data='close')
-    ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
