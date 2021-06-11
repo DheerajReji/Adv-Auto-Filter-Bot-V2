885ead9b6ce28f80ef7cf94d4108438da656ef5a
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-06-11 11:24:13 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -17,14 +17,3 @@ class Translation(object):
 <i>        
 @the_johnniewalker
 """
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/the_johnniewalker">NEGAN</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/DheerajReji/Advanced-Auto-Filter-Bot">Click Me</a></i>
-"""
